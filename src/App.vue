3bad451fe41c849790f792d5cc0ffbc0c3be4b85
Wang Qi <wangqi@Wangs-MBP.lan>
2017-03-09 19:29:53 +0800

    project file

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+
+    <h1 v-html="title"></h1>
+    <input v-model="newItem" v-on:keyup.enter="addNew" placeholder="edit here">
+    <div>{{content}}</div>
+    <section>
+      <ul>
+        <li v-for="item in items"
+        v-bind:class="{finished: item.isFinished}" v-on:click="clickFinished(item)">
+          {{item.label}}
+        </li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+import Store from './store'
+console.log(Store)
+export default {
+  data () {
+    return {
+      title: '<span>love</span>Vue.js',
+      content: 'This is my first vue menu list',
+      items: Store.fetch(),
+      newItem: ''
+    }
+  },
+  methods: {
+    clickFinished: function (item) {
+      item.isFinished = !item.isFinished
+    },
+    addNew: function () {
+      console.log(this.newItem)
+      this.items.push({
+        label: this.newItem,
+        isFinished: false
+      })
+      this.newItem = ''
+      Store.save()
+    }
+  },
+  watch: {
+    items: {
+      handler: function (items) {
+        Store.save(items)
+      },
+      deep: false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #f40;
+  margin-top: 60px;
+}
+.finished {
+  text-decoration:underline;
+  border:1px blue;
+  font-size:16px;
+}
+li{
+  cursor:pointer;
+  font-size:20px;
+  text-decoration: none;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">貂蝉d</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">吕布</a></li>
+      <li><a href="https://gitter.im/vuejs/vue" target="_blank">荆轲</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">花木兰</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">王者荣耀</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">阴阳师</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">三国杀</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">穿越火线</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: '这里放文字~'
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+#hello {
+  color:#ff00ff;
+}
+</style>
